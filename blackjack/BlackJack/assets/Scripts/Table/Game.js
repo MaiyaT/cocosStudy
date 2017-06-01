8e891626015e00eb68368720412200036a95f0e2
林宁宁 <763465697@qq.com>
2017-06-01 13:01:59 +0800

    21点-切换电脑

---
+++ blackjack/BlackJack/assets/Scripts/Table/Game.js
@@ -0,0 +1,20 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        
+        player_anchor:[cc.Node],
+        player_prefab:cc.Prefab,
+
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
---
+++ blackjack/BlackJack/assets/Scripts/Table/InGameUI.js
@@ -0,0 +1,24 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        panelChat: cc.Node,
+        panelSocial: cc.Node,
+        betStateUI: cc.Node,
+        gameStateUI: cc.Node,
+        resultTxt: cc.Label,
+        betCounter: cc.ProgressBar,
+        btnStart: cc.Node,
+        labelTotalChips: cc.Label
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
