2ee65a8519eb959969a849c219d4a8c938a8fd9a
林宁宁 <763465697@qq.com>
2017-06-02 22:11:59 +0800

    21点-Deck

--- blackjack/BlackJack/assets/Scripts/Table/game/Decks.js
+++ blackjack/BlackJack/assets/Scripts/Table/game/Decks.js
@@ -41,31 +41,32 @@ Decks.prototype.draw = function(){
     cardIds.length = len - 1;
     return result;
 };
-....
 
-cc.Class({
-    extends: cc.Component,
+module.exports = Decks;
 
-    properties: {
-        // foo: {
-        //    default: null,      // The default value will be used only when the component attaching
-        //                           to a node for the first time
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
-    },
+// cc.Class({
+//     extends: cc.Component,
 
-    // use this for initialization
-    onLoad: function () {
+//     properties: {
+//         // foo: {
+//         //    default: null,      // The default value will be used only when the component attaching
+//         //                           to a node for the first time
+//         //    url: cc.Texture2D,  // optional, default is typeof default
+//         //    serializable: true, // optional, default is true
+//         //    visible: true,      // optional, default is true
+//         //    displayName: 'Foo', // optional
+//         //    readonly: false,    // optional, default is false
+//         // },
+//         // ...
+//     },
 
-    },
+//     // use this for initialization
+//     onLoad: function () {
 
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
+//     },
 
-    // },
-});
+//     // called every frame, uncomment this function to activate update callback
+//     // update: function (dt) {
+
+//     // },
+// });
